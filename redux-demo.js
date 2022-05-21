0732e5afeedb343ac8cdea854ed847a0ca17d883
DT0998 <duongkytrieuvn@gmail.com>
2022-05-22 00:10:34 +0700

    update redux demo

--- redux-demo.js
+++ redux-demo.js
@@ -1,3 +1,23 @@
-const redux = require('redux');
+const redux = require("redux");
 
-const store = redux.configureStore();
+// reducer function
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
+
+// store
+const store = redux.createStore(counterReducer);
+// initial state
+// console.log(store.getState());
+
+// subcriber
+const counterSubscriber = () => {
+  const lastestState = store.getState();
+  console.log(lastestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type:'increment',});
